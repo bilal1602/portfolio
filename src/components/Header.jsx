60088c1ffe15055afe4d0de008281eb071f07367
bilal1602 <bilal.sajjad786@gmail.com>
2021-09-01 19:52:42 +0500

    Change: Remove comments.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
-// import { LazyLoadImage } from "react-lazy-load-image-component";
 import content from "../content";
 import Typical from "react-typical";
 import { ThemeContext } from "../contexts/ThemeContext";
-// function BgImage(props) {
-// 	return (
-// 		<div
-// 			style={{
-// 				background: "url(" + props.bg + ")",
-// 				backgroundSize: "cover",
-// 				height: "60vh",
-// 				width: "60vh",
-// 				borderRadius: 5,
-// 				zIndex: "1000",
-// 			}}
-// 		>
-// 			{/* <div
-// 				style={{
-// 					position: "absolute",
-// 					height: "60vh",
-// 					width: "60vh",
-// 					backgroundColor: props.tint,
-// 					zIndex: "1000",
-// 				}}
-// 			/> */}
-// 		</div>
-// 	);
-// }
 
 const Header = () => {
 	const {
@@ -37,10 +12,7 @@ const Header = () => {
 	return (
 		<div className="min-h-screen flex items-center justify-center">
 			<div className={"w-10/12 mx-auto flex flex-col md:flex-row-reverse items-center justify-between"}>
-				<div className={"w-full md:w-2/5"}>
-					{/* <BgImage bg={content.header.img} tint="rgba(9, 28, 41,0.7)" /> */}
-					{/* <LazyLoadImage style={{ zIndex: 1000 }} src={content.header.img} effect="blur"></LazyLoadImage> */}
-				</div>
+				<div className={"w-full md:w-2/5"}></div>
 				<div className="w-8/12 text-white font-roboto">
 					<span className="font-consolas m-0" style={{ color: color1 }}>
 						{content.header.text[0]}
--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -6,18 +6,6 @@ const NavLink = ({ link, count }) => {
 	let { theme } = useContext(ThemeContext);
 	return (
 		<ScrollLink smooth={true} to={link.to}>
-			{/* <div className="md:hidden mobile-menu">
-				<ul>
-					<li
-						className="block text-sm px-2 py-4  hover:text-color1 transition duration-200"
-						// onMouseEnter={() => setHovered(true)}
-						// onMouseLeave={() => setHovered(false)}
-						style={{ cursor: "pointer" }}
-					>
-						{link.text}
-					</li>
-				</ul>
-			</div> */}
 			<span className={"text-base py-2 px-4 cursor-pointer hover:text-color1 transition duration-200"}>
 				<span className={"text-sm"} style={{ color: theme.color1 }}>
 					0{count}
